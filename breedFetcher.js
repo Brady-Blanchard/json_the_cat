ffdd3079e6e0636fccacb0cf207071886f811eec
Brady-Blanchard <bradyjames97@hotmail.com>
2023-06-26 05:18:06 -0300

    Initial commit

---
+++ breedFetcher.js
@@ -0,0 +1,18 @@
+// require the request module
+const request = require('request');
+
+const breedName = process.argv[2];
+
+request(`https://api.thecatagpi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    const breed = data[0];
+    if (!breed) {
+      console.log(`the requested breed, ${breedName} was not found.`)
+    } else {
+      console.log(breed.description);
+    }
+  }
+});
