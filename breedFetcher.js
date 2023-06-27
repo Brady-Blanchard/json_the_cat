b6e53ac979571ad83f9e8e5b44fe9dacd307bdf3
Brady-Blanchard <bradyjames97@hotmail.com>
2023-06-27 01:44:38 -0300

    Implemented tests with mocha

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,21 @@
 // require the request module
 const request = require('request');
 
-const breedName = process.argv[2];
-
-request(`https://api.thecatagpi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const data = JSON.parse(body);
-    const breed = data[0];
-    if (!breed) {
-      console.log(`the requested breed, ${breedName} was not found.`);
+// defines the fetchBreedDescription function that takes a breed name and a callback function
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
     } else {
-      console.log(breed.description);
+      const data = JSON.parse(body);
+      const breed = data[0];
+      if (!breed) {
+        callback(`the requested breed, ${breedName} was not found.`, null);
+      } else {
+        callback(null, breed.description);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
---
+++ index.js
@@ -0,0 +1,12 @@
+// requires the fetchBreedDescription function from the breedfetcher.js file
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
